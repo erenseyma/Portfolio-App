b2531f700472fc2b9b4cd66786b8ecfd24f80cde
erenseyma <erenn.seyma@gmail.com>
2025-03-20 02:03:32 +0300

    Updates

---
+++ PortfolioApp/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioApp.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- PortfolioApp/obj/Debug/net8.0/PortfolioApp.AssemblyInfo.cs
+++ PortfolioApp/obj/Debug/net8.0/PortfolioApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0113c0936e69ea3cbf1b6c18c713d5aa7cc6eb06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ca4ce970394c6b7e8acc008c8962f8180cf97e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
